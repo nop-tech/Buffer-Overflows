340976ec37239d4c138f63d57c1187b0c26bce0c
nop <80179861+nop-tech@users.noreply.github.com>
2021-11-13 17:49:12 +0100

    Update fuzzing.py

--- Basic Scripts/fuzzing.py
+++ Basic Scripts/fuzzing.py
@@ -5,7 +5,7 @@ ip = '10.0.2.57'	# Change this
 port = 1337		# Change this
 
 prefix = b''
-buffer += b'A' * 100
+buffer = b'A' * 100
 
 payload = prefix + buffer 
 
@@ -20,7 +20,7 @@ while True:
             s.send(payload)
             s.recv(1024)
     except:
-        print('Fuzzing crashed at {}'.format(len(payload))
+        print('Fuzzing crashed at {}'.format(len(payload)))
         sys.exit(0)
     buffer += b'A' * 100
     time.sleep(1)
